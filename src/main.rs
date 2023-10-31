4691797bbe258dc01f59909ab8ec3fba41117529
angelsflyinhell <angelsflyinhell@namespace.media>
2023-10-31 16:58:59 +0100

    big number support !

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,25 @@ struct KeyPair {
 }
 
 fn main() {
-    let range_start = 1;
-    let range_end = 50;
+    println!("Diffie-Hellman key exchange");
     let time_start = std::time::Instant::now();
 
-    let p = get_prime(range_start, range_end);
-    let g = get_prime(range_start, range_end);
-    println!("took {}ms since start | p: {}, g: {}", time_start.elapsed().as_millis(), p, g);
+    let p = get_prime();
+    let g = get_prime();
+    println!("p: {}, g: {}", p, g);
 
     // Alice and Bob or whatever, just 2 clients communicating
     let client_one = KeyPair {
         public_key_p: p,
         public_key_g: g,
-        private_key: get_prime(range_start, range_end),
+        private_key: get_prime(),
     };
     println!("Client 1 private key: {}", client_one.private_key);
 
     let client_two = KeyPair {
         public_key_p: p,
         public_key_g: g,
-        private_key: get_prime(range_start, range_end),
+        private_key: get_prime(),
     };
     println!("Client 2 private key: {}", client_two.private_key);
 
@@ -43,21 +42,23 @@ fn main() {
 
     let result_key_two = generate_result_key(shared_key_one, &client_two);
     println!("Client 2 result key: {}", result_key_two);
+
+    println!("took {}ms since start", time_start.elapsed().as_millis());
 }
 
 fn generate_shared_key(client: &KeyPair) -> BigInt {
-    return BigInt::from(num::pow(client.public_key_g, client.private_key) % client.public_key_p);
+    return bigint_pow(BigInt::from(client.public_key_g), client.private_key) % BigInt::from(client.public_key_p);
 }
 
 fn generate_result_key(shared_key: BigInt, client: &KeyPair) -> BigInt {
-    return BigInt::from(num::pow(shared_key, client.private_key) % client.public_key_p);
+    return bigint_pow(shared_key, client.private_key) % BigInt::from(client.public_key_p);
 }
 
 // random and large prime number
-fn get_prime(range_start: usize, range_end: usize) -> usize {
-    let mut prime: usize = 0;
+fn get_prime() -> usize {
+    let mut prime: usize;
     loop {
-        prime = rand::random::<usize>() % range_end + range_start;
+        prime = rand::random::<i8>() as usize;
         if is_prime_number(prime) {
             break;
         }
@@ -72,4 +73,20 @@ fn is_prime_number(number: usize) -> bool {
         }
     }
     return true;
+}
+
+/*
+    quick pow function according to
+    https://stackoverflow.com/questions/101439/the-most-efficient-way-to-implement-an-integer-based-power-function-powint-int
+*/
+fn bigint_pow(mut base: BigInt, mut exponent: usize) -> BigInt {
+    let mut result = BigInt::from(1);
+    for _ in 0..exponent {
+        if exponent & 1 as usize == 1 {
+            result *= base.clone();
+        }
+        exponent >>= 1;
+        base *= base.clone();
+    }
+    return result;
 }
